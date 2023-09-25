928bbd209ffc863d9ff20e7e60fb15e33378c3ed
Sateesh Kumbhakar <sateeshkumbhakar113@gmail.com>
2023-09-25 17:49:00 +0530

    update

--- src/app/components/bill/bill.component.ts
+++ src/app/components/bill/bill.component.ts
@@ -1,20 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { EmpLoginService } from 'src/app/emp-login.service';
+import { Bill } from 'src/app/model/bill';
 import { BillService } from 'src/app/myservies/bill.service';
+
 
 @Component({
   selector: 'app-bill',
   templateUrl: './bill.component.html',
-  styleUrls: ['./bill.component.css']
+  styleUrls: ['./bill.component.css',"../customer/customerOps/all-customer/all-customer.component.css",'../../app.component.css']
 })
 export class BillComponent implements OnInit {
 
-  constructor(private billService:BillService,private loginServie:EmpLoginService) { }
+  bills:Bill []= [];
+
+  constructor(private billService:BillService,private loginServie:EmpLoginService,private router:Router) { }
 
   ngOnInit(): void {
     this.loginServie.isAuthenticate = true;
+    this.getAllBill();
   }
 
-  
+
+  getAllBill(){
+    this.billService.getAllBill().subscribe((res:any)=>{    
+      console.log(res);
+     this.bills= res;
+    });  
+  }
+
+  navigateToBillDetails(billId:number){
+   console.log(billId);
+   this.router.navigate(["/bill-details",billId]);
+  }
+
+  navigateToDeleteBill(customerId:number,billId:number){
+    let confirmation = window.confirm('Are you sure you want to delete this record?');
+    if (confirmation) {
+      console.log("clicked");
+      this.billService.deleteBill(customerId,billId).subscribe((res)=>{
+       console.log(res);
+       this.router.navigate(["/bill"]);
+      })
+    }
+
+  }
+  navigateToUpdateBill(billId:number){
+
+
+  }
+
   
 }
